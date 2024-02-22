7ebea41237226caae0fea1e48113a25716f85ab5
Danilo Santos <danilosantosgit@gmail.com>
2024-02-22 09:49:22 -0300

    ADD scripts from desafio1.js and desafio2.js. (Aula 05)

---
+++ desafios-logica-programacao-1/aula-05/desafio2.js
@@ -0,0 +1,6 @@
+// Desafio 2
+// - Números Primos
+// ● Crie um programa que peça ao usuário para inserir um número e
+// use um loop while para verificar se o número é primo ou não. Um
+// número primo é aquele que só é divisível por 1 e por ele mesmo.
+// Imprima se o número é primo ou não.
